76af9ac4138d19162514ec83099f128c25b06466
sandovalmelo <juniormelo777@gmail.com>
2026-04-27 17:38:34 -0300

    feat: add database createpool

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
 	uri: process.env.DATABASE_URL,
 	ssl: {
 		rejectUnauthorized: false
