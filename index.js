08845773411a80b80dd8d3e82e108c3e8e39c258
sandovalmelo <juniormelo777@gmail.com>
2026-04-27 18:01:21 -0300

    feat: edit cors origin

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import dotenv from "dotenv";
 import { db } from "./db.js";
 
 const app = express();
-app.use(cors());
+app.use(
+	cors({
+		origin: "*"
+	})
+);
 app.use(express.json());
 dotenv.config();
 
