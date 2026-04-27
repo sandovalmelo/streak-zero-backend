d31fea9468973eb754d2a6704cffa271f610f453
sandovalmelo <juniormelo777@gmail.com>
2026-04-27 16:29:52 -0300

    feat: add root route

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-	res.json({ messagge: "Olá Júnior" });
+	res.send("API Rodando");
 });
 
 app.get("/habits", async (req, res) => {
