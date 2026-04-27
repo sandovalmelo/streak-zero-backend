ee578952e077a65b308c4d961d18197dd5d7e383
sandovalmelo <juniormelo777@gmail.com>
2026-04-27 17:21:38 -0300

    feat: add database url connection

--- db.js
+++ db.js
@@ -2,4 +2,9 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const db = mysql.createPool(process.env.DATABASE_URL);
+export const db = mysql.createConnection({
+	uri: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false
+	}
+});
